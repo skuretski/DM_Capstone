e4de6113843fb1ea9a0494765313b9226210dbd7
Susie <skuretski@gmail.com>
2021-07-21 20:23:49 -0500

    WIP task 6

--- Project/Task_6/main.py
+++ Project/Task_6/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import GaussianNB
+from sklearn.datasets import load_wine
 
 def get_word_vector():
   corpus = []
@@ -9,19 +10,38 @@ def get_word_vector():
     for line in f:
       line = re.sub(r'[^A-Za-z\s]', '', line)
       corpus.append(line)
-  
+  f.close()
   vectorizer = CountVectorizer()
   X = vectorizer.fit_transform(corpus)
   return X.toarray()
 
 def get_y():
-  return
+  train = []
+  test = []
+  with open('../Task_6/data/hygiene.dat.labels', 'r') as f:
+    for line in f:
+      line = list(line)
+      for ch in line:
+        if ch == '1' or ch == '0':
+          train.append(int(ch))
+        else:
+          test.append(None)
+  f.close()
+  return train, test
 
 def main():
   df = pd.read_csv('../Task_6/data/hygiene.dat.additional', sep=",", usecols=[0,1,2,3], names=['Categories', 'Zip_Code', 'Num_Reviews', 'Rating'])
+  vectorizer = CountVectorizer()
+  categories = []
+  for index, row in df.iterrows():
+    categories.append(row['Categories'])
+  X = vectorizer.fit_transform(categories)
+  
+
   word_vector = get_word_vector()
-
-
+  train, test = get_y()
+  features, target = load_wine(return_X_y=True)
+  print(features)
   return
 
 

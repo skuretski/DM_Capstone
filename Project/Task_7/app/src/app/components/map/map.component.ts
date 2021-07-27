fa072397cdd19fc67916fac88083056b5d3d199d
Susie <skuretski@gmail.com>
2021-07-27 18:37:42 -0500

    WIP maps

--- Project/Task_7/app/src/app/components/map/map.component.ts
+++ Project/Task_7/app/src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import * as topojson from "topojson-client";
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,16 +8,49 @@ import * as d3 from 'd3';
 })
 export class MapComponent implements OnInit {
   svg: any;
-  data: any;
+  data_businesses: any;
+  data_states: any;
+  path: any;
+  projection: any;
+  map: any;
+
+  width: number = 1000;
+  height: number = 1000;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
     this.svg = d3.select("#mapviz")
       .append("svg")
+      .attr("width", this.width)
+      .attr("height", this.height);
+
+    this.projection = d3.geoAlbersUsa().scale(1000);
+    this.path = d3.geoPath().projection(this.projection)
+
+    await Promise.all([
+      d3.json('../../../assets/data/states.json'),
+      d3.json('../../../assets/data/geojson.json')
+    ]).then(([data1, data2]) => {
+      this.data_states = data1
+      this.data_businesses = data2
+      this.svg.append("g")
+      this.makeMap()
+    })
+  }
+
+  makeMap() {
+    this.svg.select("g")
+      .style("fill", "#fff")
+      .style("stroke", "aaa")
+      .style("stroke-width", "1px")
+      .append('path')
+      .datum(topojson.feature(this.data_states, this.data_states.objects.usStates))
+      .attr('d', this.path)
   }
 
 }
